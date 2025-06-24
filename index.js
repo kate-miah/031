6ecab8faaa6c0f574b96f77f3d9237d44fb0f4eb
kate-miah <ekaterin89@gmail.com>
2025-06-23 22:34:30 -0500

    console task resolve

---
+++ index.js
@@ -0,0 +1,15 @@
+let age = 20;
+// "Вам ... років"
+
+let consoleMessage;
+
+consoleMessage = 'Вам ' + age + ' років';
+
+console.log(consoleMessage);
+
+//console.log('hello')\
+
+
+let result = 4;
+let mathResult = '2 + 2 = ' + result;
+console.log(mathResult);
