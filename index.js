f677ed5f1f7af0afbbcae604095ce0fae3db6aae
kate-miah <ekaterin89@gmail.com>
2025-06-24 21:44:14 -0500

    and, or

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-let age = 20;
-// "Вам ... років"
+// let age = 20;
+// // "Вам ... років"
 
-let consoleMessage;
+// let consoleMessage;
 
-consoleMessage = 'Вам ' + age + ' років';
+// consoleMessage = 'Вам ' + age + ' років';
 
-console.log(consoleMessage);
+// console.log(consoleMessage);
 
-//console.log('hello')\
+// //console.log('hello')\
 
 
-let result = 4;
-let mathResult = '2 + 2 = ' + result;
-console.log(mathResult);
+// let result = 4;
+// let mathResult = '2 + 2 = ' + result;
+// console.log(mathResult);
+
+
+let age = 30;
+age >= 20 && age <30;
+console.log(age);
+
+let age1 = 35;
+age1 >= 20 || age1 <= 40;
+console.log(age1);
+
+let userName = '';
+let consoleMessage = 'Hello ' + (userName || 'Анонім');
+console.log(consoleMessage); 
